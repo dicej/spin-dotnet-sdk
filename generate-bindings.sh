48fb582128ddc3e53099820f3fb8b6e27f0ed0db
Joel Dice <joel.dice@fermyon.com>
2024-08-02 17:29:59 -0600

    update to wasi-sdk 24 and wit-bindgen 0.29.0
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-# TODO: update to next release containing https://github.com/bytecodealliance/wit-bindgen/pull/1017
-cargo install --locked --no-default-features --features csharp --git https://github.com/dicej/wit-bindgen --rev e9430c5a wit-bindgen-cli --root $(pwd)
+cargo install --locked --no-default-features --features csharp --version 0.29.0 wit-bindgen-cli --root $(pwd)
 ./bin/wit-bindgen c-sharp -w spin-http -r native-aot wit
 rm SpinHttpWorld_component_type.o SpinHttpWorld_cabi_realloc.c SpinHttpWorld_wasm_import_linkage_attribute.cs
