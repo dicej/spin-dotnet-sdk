bc618d4277999c9882f9d4ec2162075de6180f25
Joel Dice <joel.dice@fermyon.com>
2024-08-13 15:45:06 -0600

    add `http-aspnetcore` example
    This doesn't work yet.  Stay tuned.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

---
+++ samples/http-aspnetcore/App.cs
@@ -0,0 +1,63 @@
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Spin.Http;
+using SpinHttpWorld.wit.imports.wasi.http.v0_2_0;
+
+namespace SpinHttpWorld.wit.exports.wasi.http.v0_2_0;
+
+public class IncomingHandlerImpl : IIncomingHandler
+{
+    /// <summary>Handle the specified incoming HTTP request and send a response
+    /// via `responseOut`.</summary>
+    public static void Handle(ITypes.IncomingRequest request, ITypes.ResponseOutparam responseOut)
+    {
+        var builder = WebApplication.CreateBuilder(new string[0]);
+        builder.Services.AddSingleton<IServer, WasiHttpServer>();
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+        app.UseStaticFiles();
+        app.MapRazorPages();
+
+        app.MapGet("/", () => "Hello, world! See also: /weatherforecast and /myrazorpage");
+
+        app.MapGet(
+            "/weatherforecast",
+            () =>
+            {
+                var summaries = new[]
+                {
+                    "Freezing",
+                    "Bracing",
+                    "Chilly",
+                    "Cool",
+                    "Mild",
+                    "Warm",
+                    "Balmy",
+                    "Hot",
+                    "Sweltering",
+                    "Scorching"
+                };
+                var forecast = Enumerable
+                    .Range(1, 5)
+                    .Select(index => new
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TempC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            }
+        );
+
+        Func<Task> task = async () =>
+        {
+            await app.StartAsync();
+            await WasiHttpServer.HandleRequestAsync(request, responseOut);
+        };
+        RequestHandler.Run(task());
+    }
+}
---
+++ samples/http-aspnetcore/WasiHttpServer.cs
@@ -0,0 +1,258 @@
+using System.IO.Pipelines;
+using System.Text;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Hosting;
+using Spin.Http;
+using SpinHttpWorld.wit.imports.wasi.http.v0_2_0;
+
+namespace SpinHttpWorld.wit.exports.wasi.http.v0_2_0;
+
+public class WasiHttpServer : IServer
+{
+    private static Func<ITypes.IncomingRequest, ITypes.ResponseOutparam, Task>? _handleRequest;
+
+    public IFeatureCollection Features { get; } = new FeatureCollection();
+
+    public WasiHttpServer(IHostApplicationLifetime lifetime) { }
+
+    public Task StartAsync<TContext>(
+        IHttpApplication<TContext> application,
+        CancellationToken cancellationToken
+    )
+        where TContext : notnull
+    {
+        _handleRequest = async (request, responseOut) =>
+        {
+            var requestContext = new RequestContext(request, responseOut);
+
+            var requestFeatures = new FeatureCollection();
+            requestFeatures[typeof(IHttpRequestFeature)] = requestContext;
+            requestFeatures[typeof(IHttpResponseFeature)] = requestContext;
+            requestFeatures[typeof(IHttpResponseBodyFeature)] = requestContext;
+
+            var ctx = application.CreateContext(requestFeatures);
+            try
+            {
+                await application.ProcessRequestAsync(ctx);
+                application.DisposeContext(ctx, null);
+            }
+            catch (Exception ex)
+            {
+                application.DisposeContext(ctx, ex);
+                throw;
+            }
+        };
+
+        return Task.CompletedTask;
+    }
+
+    public static async Task HandleRequestAsync(
+        ITypes.IncomingRequest request,
+        ITypes.ResponseOutparam responseOut
+    )
+    {
+        if (_handleRequest is not null)
+        {
+            await _handleRequest(request, responseOut);
+        }
+        else
+        {
+            throw new Exception("WasiHttpServer not yet initialized");
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    public void Dispose() { }
+
+    static string MethodString(ITypes.Method method)
+    {
+        switch (method.Tag)
+        {
+            case ITypes.Method.GET:
+                return "GET";
+            case ITypes.Method.HEAD:
+                return "HEAD";
+            case ITypes.Method.POST:
+                return "POST";
+            case ITypes.Method.PUT:
+                return "PUT";
+            case ITypes.Method.DELETE:
+                return "DELETE";
+            case ITypes.Method.CONNECT:
+                return "CONNECT";
+            case ITypes.Method.OPTIONS:
+                return "OPTIONS";
+            case ITypes.Method.TRACE:
+                return "TRACE";
+            case ITypes.Method.PATCH:
+                return "PATCH";
+            case ITypes.Method.OTHER:
+                return method.AsOther;
+            default:
+                throw new Exception("unreachable code");
+        }
+    }
+
+    class RequestContext : IHttpRequestFeature, IHttpResponseFeature, IHttpResponseBodyFeature
+    {
+        private List<(Func<object, Task>, object)> _onStartingCallbacks = new();
+        private List<(Func<object, Task>, object)> _onCompletedCallbacks = new();
+        private ITypes.IncomingBody _requestBody;
+        private ITypes.ResponseOutparam _responseOut;
+        private ITypes.OutgoingBody? _responseBody;
+        private Stream? _stream;
+        private PipeWriter? _writer;
+
+        internal RequestContext(ITypes.IncomingRequest request, ITypes.ResponseOutparam responseOut)
+        {
+            var headers = new HeaderDictionary();
+            foreach ((var key, var value) in request.Headers().Entries())
+            {
+                headers.Add(key, Encoding.UTF8.GetString(value));
+            }
+
+            var url = request.PathWithQuery();
+            if (url is null)
+            {
+                url = "/";
+            }
+
+            var queryStartPos = url.IndexOf('?');
+            Path = queryStartPos < 0 ? url : url.Substring(0, queryStartPos);
+            QueryString = queryStartPos < 0 ? string.Empty : url.Substring(queryStartPos);
+            Method = MethodString(request.Method());
+            ((IHttpRequestFeature)this).Headers = headers;
+            _requestBody = request.Consume();
+            ((IHttpRequestFeature)this).Body = new InputStream(_requestBody.Stream());
+
+            _responseOut = responseOut;
+        }
+
+        public string Protocol { get; set; } = "HTTP/1.1";
+        public string Scheme { get; set; } = "http";
+        public string Method { get; set; } = "GET";
+        public string PathBase { get; set; } = string.Empty;
+        public string Path { get; set; } = "/";
+        public string QueryString { get; set; } = string.Empty;
+        public string RawTarget
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        // TODO: Prohibit changing the status code or response headers if
+        // `_stream is not null` (i.e. if we've already started sending the
+        // response)
+        public int StatusCode { get; set; } = 200;
+        public string? ReasonPhrase { get; set; }
+
+        public bool HasStarted { get; }
+
+        public Stream Stream
+        {
+            get
+            {
+                if (_stream is null)
+                {
+                    var responseHeaders = new List<(string, byte[])>();
+                    foreach (var pair in ((IHttpResponseFeature)this).Headers)
+                    {
+                        responseHeaders.Add((pair.Key, Encoding.UTF8.GetBytes(pair.Value.ToString())));
+                    }
+
+                    var response = new ITypes.OutgoingResponse(
+                        ITypes.Fields.FromList(responseHeaders)
+                    );
+                    response.SetStatusCode((ushort)StatusCode);
+                    _responseBody = response.Body();
+                    ITypes.ResponseOutparam.Set(
+                        _responseOut,
+                        Result<ITypes.OutgoingResponse, ITypes.ErrorCode>.ok(response)
+                    );
+                    _stream = new OutputStream(_responseBody.Write());
+                }
+
+                return _stream;
+            }
+        }
+
+        public PipeWriter Writer
+        {
+            get
+            {
+                if (_writer is null)
+                {
+                    _writer = PipeWriter.Create(Stream);
+                }
+
+                return _writer;
+            }
+        }
+
+        IHeaderDictionary IHttpRequestFeature.Headers { get; set; } = new HeaderDictionary();
+        IHeaderDictionary IHttpResponseFeature.Headers { get; set; } = new HeaderDictionary();
+
+        Stream IHttpRequestFeature.Body { get; set; } = default!;
+        Stream IHttpResponseFeature.Body
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public async Task CompleteAsync()
+        {
+            ITypes.IncomingBody.Finish(_requestBody);
+
+            Stream.Dispose();
+
+            ITypes.OutgoingBody.Finish(_responseBody!, null);
+
+            foreach (var c in _onCompletedCallbacks)
+            {
+                await c.Item1(c.Item2);
+            }
+        }
+
+        public void DisableBuffering()
+        {
+            // We don't buffer anyway
+        }
+
+        public void OnCompleted(Func<object, Task> callback, object state)
+        {
+            _onCompletedCallbacks.Add((callback, state));
+        }
+
+        public void OnStarting(Func<object, Task> callback, object state)
+        {
+            _onStartingCallbacks.Add((callback, state));
+        }
+
+        public async Task SendFileAsync(
+            string path,
+            long offset,
+            long? count,
+            CancellationToken cancellationToken = default
+        )
+        {
+            if (offset != 0 || count is not null)
+            {
+                throw new Exception("TODO: handle offset and count");
+            }
+            using var file = File.OpenRead(path);
+            await file.CopyToAsync(Stream);
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var c in _onStartingCallbacks)
+            {
+                await c.Item1(c.Item2);
+            }
+        }
+    }
+}
