ae752297b08a25806ae41afb5221a6adfb2c85b6
Thorsten Hans <thorsten.hans@fermyon.com>
2024-08-14 16:46:12 +0200

    chore: add useful default values for InferencingParams
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- src/LLM/InferencingParams.cs
+++ src/LLM/InferencingParams.cs
@@ -1,17 +1,19 @@
+using SpinHttpWorld.wit.imports.fermyon.spin.v2_0_0;
+
 namespace Spin.SDK.LLM;
 
 public class InferencingParams
 {
-    public uint MaxTokens { get; set; }
-    public uint TopK { get; set; }
-    public float TopP { get; set; }
-    public float Temperature { get; set; }
-    public float RepeatPenalty { get; set; }
-    public uint RepeatPenaltyLastNTokenCount { get; set; }
+    public uint MaxTokens { get; set; } = 100;
+    public uint TopK { get; set; } = 40;
+    public float TopP { get; set; } = 0.9f;
+    public float Temperature { get; set; } = 0.8f;
+    public float RepeatPenalty { get; set; } = 1.1f;
+    public uint RepeatPenaltyLastNTokenCount { get; set; } = 64;
 
-    internal SpinHttpWorld.wit.imports.fermyon.spin.v2_0_0.ILlm.InferencingParams Lower()
+    internal ILlm.InferencingParams Lower()
     {
-        return new SpinHttpWorld.wit.imports.fermyon.spin.v2_0_0.ILlm.InferencingParams(
+        return new ILlm.InferencingParams(
             MaxTokens, RepeatPenalty, RepeatPenaltyLastNTokenCount, Temperature, TopK, TopP);
     }
 }
