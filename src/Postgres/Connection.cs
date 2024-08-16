3161a4b4ceb6fffa15267a9788e85ae8912272a6
Thorsten Hans <thorsten.hans@fermyon.com>
2024-08-16 12:15:29 +0200

    feat: Add Postgres APIs
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

---
+++ src/Postgres/Connection.cs
@@ -0,0 +1,35 @@
+using Spin.SDK.RdbmsTypes;
+using SpinHttpWorld.wit.imports.fermyon.spin.v2_0_0;
+
+namespace Spin.SDK.Postgres;
+
+public class Connection : IDisposable
+{
+    private readonly IPostgres.Connection _connection;
+
+    private Connection(IPostgres.Connection connection)
+    {
+        _connection = connection;
+    }
+
+    public static Connection Open(string connectionString)
+    {
+        return new Connection(IPostgres.Connection.Open(connectionString));
+    }
+
+    public ulong Execute(string statement, List<ParameterValue> parameters)
+    {
+        return _connection.Execute(statement, parameters.Select(p => p.Value).ToList());
+    }
+
+    public RowSet Query(string query, List<ParameterValue> parameters)
+    {
+        var rowSet = _connection.Query(query, parameters.Select(p => p.Value).ToList());
+        return RowSet.From(rowSet);
+    }
+
+    public void Dispose()
+    {
+        _connection.Dispose();
+    }
+}
