b4eb4e6b18342ac694476edea14362f69fbe1dfc
Matthew Fisher <matt.fisher@fermyon.com>
2024-08-19 10:26:39 -0700

    bump to wit-bindgen 0.30.0 (#9)
    * bump to wit-bindgen 0.30.0
    
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>
    
    * make the version explicit
    
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>
    
    ---------
    
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/generate-bindings.sh
+++ src/generate-bindings.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-# TODO: update to next wit-bindgen release once
-# https://github.com/bytecodealliance/wit-bindgen/pull/1026 and
-# https://github.com/bytecodealliance/wit-bindgen/pull/1027 are merged and
-# released
-cargo install --locked --no-default-features --features csharp --git https://github.com/dicej/wit-bindgen --branch csharp-public-imports-tmp wit-bindgen-cli --root $(pwd)
+cargo install --locked --no-default-features --features csharp --version 0.30.0 wit-bindgen-cli --root $(pwd)
 ./bin/wit-bindgen c-sharp -w spin-http -r native-aot wit
 rm SpinHttpWorld_wasm_import_linkage_attribute.cs
