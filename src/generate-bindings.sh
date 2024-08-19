840f91e74d42c549038e109c1a9c25c619811d04
Matthew Fisher <matt.fisher@fermyon.com>
2024-08-19 09:30:24 -0700

    make the version explicit
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/generate-bindings.sh
+++ src/generate-bindings.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-cargo install --locked --no-default-features --features csharp wit-bindgen-cli --root $(pwd)
+cargo install --locked --no-default-features --features csharp --version 0.30.0 wit-bindgen-cli --root $(pwd)
 ./bin/wit-bindgen c-sharp -w spin-http -r native-aot wit
 rm SpinHttpWorld_wasm_import_linkage_attribute.cs
